317d21e0c4ebf15a475142ff1e9068139d59facf
anto-d88 <antonio-123@hotmail.be>
2026-04-28 22:23:37 +0200

    checkout repared

--- routes/order.js
+++ routes/order.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
+router.get('/checkout',orderController.getCheckoutPage);
 router.post('/create-checkout-session', orderController.createCheckoutSession);
 router.get('/success', orderController.handleSuccess);
 router.get('/cancel', orderController.handleCancel);
