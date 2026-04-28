6d0eed010eb41b96ed93e107581252105d1487cd
anto-d88 <antonio-123@hotmail.be>
2026-04-28 22:36:32 +0200

    fix order routes handlers

--- routes/order.js
+++ routes/order.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-router.get('/checkout',orderController.getCheckoutPage);
+console.log(orderController);
+
+router.get('/checkout', orderController.getCheckoutPage);
+
 router.post('/create-checkout-session', orderController.createCheckoutSession);
-router.get('/success', orderController.handleSuccess);
-router.get('/cancel', orderController.handleCancel);
+
+router.get('/success', orderController.handlePaymentSuccess);
+
+router.get('/cancel', orderController.handlePaymentCancel);
 
 module.exports = router;
